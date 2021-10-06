78a73a5d9072cba15336a262d3820d2315b0e051
Andrei Roh <andrei_roh@tut.by>
2021-10-06 09:47:11 +0300

    feat: added some tests for app components
    Add enzyme tests for <App /> and <Main /> components

---
+++ src/components/Main.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Main from './Main';
+
+describe('<Main />', () => {
+  const main = shallow(<Main />);
+  it('renders properly', () => {
+    expect(main).toMatchSnapshot();
+  });
+});
